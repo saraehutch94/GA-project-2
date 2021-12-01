36cf8c6484451d9c7a392a34a6409132229a55c8
Sara Hutchinson <sarahutchinson@Saras-MacBook-Pro-2.local>
2021-11-30 20:20:26 -0500

    wip: add server.js boilerplate

---
+++ server.js
@@ -0,0 +1,13 @@
+// Require dependencies
+
+// Initialize the application
+
+// Configure application settings
+
+// Set up port value
+
+// Mount middleware
+
+// Mount routes
+
+// Tell app to listen for requests from client/browser
