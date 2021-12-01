f04ff6c828e7f83451be8b1ba845f1b47b8e3e27
Sara Hutchinson <sarahutchinson@Saras-MacBook-Pro-2.local>
2021-11-30 21:43:19 -0500

    create wineSchema from mongoose Schema

--- models/wine.js
+++ models/wine.js
@@ -3,3 +3,12 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+// Create mongoose Schema
+
+const wineSchema = new Schema(
+    {
+        varietal: String,
+        region: String,
+        description: String
+    }, { timestamps: true },
+);
