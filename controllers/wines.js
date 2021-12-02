2e9418ea9e3390f25b926264256a0d6b3fc87ec5
Sara Hutchinson <sarahutchinson@Saras-MacBook-Pro-2.local>
2021-12-02 11:33:17 -0500

    add ejs partials for head and footer

--- controllers/wines.js
+++ controllers/wines.js
@@ -22,36 +22,42 @@ wineRouter.get("/seed", (req, res) => {
 
 // landing page route
 wineRouter.get("/", (req, res) => {
-    res.render("landing.ejs");
+    res.render("landing.ejs", {tabTitle: "Home"});
 });
 
 // shades route
 wineRouter.get("/shades", (req, res) => {
-    res.render("shades.ejs");
+    res.render("shades.ejs", {tabTitle: "Shades"});
 });
 
 // red wine (index) route
 wineRouter.get("/redIndex", (req, res) => {
     Wine.find({shade: "Red"}, (error, allReds) => {
-        res.render("redIndex.ejs", {allReds});
+        res.render("redIndex.ejs", {
+            allReds,
+            tabTitle: "Vino Rosso",
+        });
     });
 });
 
 // white wine (index) route
 wineRouter.get("/whiteIndex", (req, res) => {
     Wine.find({shade: "White"}, (error, allWhites) => {
-        res.render("whiteIndex.ejs", {allWhites});
+        res.render("whiteIndex.ejs", {
+            allWhites,
+            tabTitle: "Vino Bianco",
+        });
     });
 });
 
 // new red route
 wineRouter.get("/redIndex/new", (req, res) => {
-    res.render("newRed.ejs");
+    res.render("newRed.ejs", {tabTitle: "New Vino Rosso"});
 });
 
 // new white route
 wineRouter.get("/whiteIndex/new", (req, res) => {
-    res.render("newWhite.ejs");
+    res.render("newWhite.ejs", {tabTitle: "New Vino Bianco"});
 });
 
 // delete route
@@ -98,14 +104,20 @@ wineRouter.post("/whiteIndex", (req, res) => {
 // edit route
 wineRouter.get("/:id/edit", (req, res) => {
     Wine.findById(req.params.id, (error, foundWine) => {
-        res.render("edit.ejs", {foundWine});
+        res.render("edit.ejs", {
+            foundWine,
+            tabTitle: "Edit Vino",
+        });
     });
 });
 
 // show route
 wineRouter.get("/:id", (req, res) => {
     Wine.findById(req.params.id, (error, foundWine) => {
-        res.render("show.ejs", {foundWine});
+        res.render("show.ejs", {
+            foundWine,
+            tabTitle: foundWine.varietal,
+        });
     });
 });
 
