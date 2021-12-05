ed25ba912b42f5c3bb49c7843ad7ed8222e5d914
Sara Hutchinson <sarahutchinson@Saras-MacBook-Pro-2.local>
2021-12-05 16:39:34 -0500

    create user schema and export user model

--- models/user.js
+++ models/user.js
@@ -6,3 +6,13 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
+// Define user Schema
+
+const userSchema = new Schema({
+    firstName: String,
+    lastName: String,
+    email: String,
+    password: String,
+}, { timestamps : true });
+
+module.exports = mongoose.model("User", userSchema);
