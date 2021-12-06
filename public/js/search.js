9892582cb09b4c545d0ab99b6d79ec6ac523360e
Sara Hutchinson <sarahutchinson@Saras-MacBook-Pro-2.local>
2021-12-06 14:00:47 -0500

    logic for no search results

--- public/js/search.js
+++ public/js/search.js
@@ -7,21 +7,25 @@ inputElement.addEventListener("focus", handleReset);
 
 async function handleClick() {
     const searchTerm = inputElement.value;
-    if (!searchTerm) return alert("Please enter valid search term");
+    if (!searchTerm) return alert("Please enter a search term.");
     const response = await fetch("/vino-italiano/search?term=" + searchTerm);
     const data = await response.json();
 
-    const list = data.results.map(wine => {
-        return `<li>
-                    <a href="/vino-italiano/${wine._id}">
-                        ${wine.varietal}
-                    </a>
-                </li>`
-    }).join("");
-
-    ulElement.innerHTML = list;
+    if(data.results.length === 0) {
+        ulElement.innerHTML = `<li>No Search Results</li>`
+    } else {
+        const list = data.results.map(wine => {
+            return `<li>
+                        <a href="/vino-italiano/${wine._id}">
+                            ${wine.varietal}
+                        </a>
+                    </li>`
+        }).join("");
+    
+        ulElement.innerHTML = list;
+    }
     inputElement.value = "";
-};
+}
 
 function handleReset() {
     ulElement.innerHTML = "";
