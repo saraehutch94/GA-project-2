d2627ef66a68b8751d3933a0f79eb782a5fc335a
Sara Hutchinson <sarahutchinson@Saras-MacBook-Pro-2.local>
2021-12-07 12:34:27 -0500

    take out list items of search results, sub in div and span

--- public/js/search.js
+++ public/js/search.js
@@ -1,32 +1,32 @@
 const buttonElement = document.querySelector("button");
 const inputElement = document.querySelector("input");
-const ulElement = document.querySelector("ul");
+const divElement = document.getElementById("search-results");
 
 buttonElement.addEventListener("click", handleClick);
 inputElement.addEventListener("focus", handleReset);
 
 async function handleClick() {
     const searchTerm = inputElement.value;
-    if (!searchTerm) return ulElement.innerHTML = "<li style='color: red;'>Please enter a search term</li>"
+    if (!searchTerm) return divElement.innerHTML = "<div style='color: red;'>Please enter a search term</div>"
     const response = await fetch("/vino-italiano/search?term=" + searchTerm);
     const data = await response.json();
 
     if(data.results.length === 0) {
-        ulElement.innerHTML = `<li style="color: red;">No Search Results</li>`
+        divElement.innerHTML = `<div style="color: red;">No Search Results</div>`
     } else {
         const list = data.results.map(wine => {
-            return `<li class="uppercase-word">
-                        <a class="search-list-link" href="/vino-italiano/${wine._id}">
+            return `<span class="uppercase-word flex-search-results">
+                        <div class="flex-search-item"><a class="search-list-link" href="/vino-italiano/${wine._id}">
                             ${wine.varietal}
-                        </a>
-                    </li>`
+                        </a></div>
+                    </span>`
         }).join("");
     
-        ulElement.innerHTML = list;
+        divElement.innerHTML = list;
     }
     inputElement.value = "";
 }
 
 function handleReset() {
-    ulElement.innerHTML = "";
+    divElement.innerHTML = "";
 };
