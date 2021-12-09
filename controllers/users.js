993c33ec51b061724e2d0fd508019fa8683b9caf
Sara Hutchinson <sarahutchinson@Saras-MacBook-Pro-2.local>
2021-12-09 15:23:16 -0500

    comment to check for new remote repo location

--- controllers/users.js
+++ controllers/users.js
@@ -58,6 +58,8 @@ usersRouter.get("/logout", (req, res) => {
     });
 });
 
+// comment to check for new repo pushes
+
 // dashboard route
 usersRouter.get("/dashboard", (req, res) => {
     if(!req.session.user) return res.redirect("/vino-italiano/users/login");
