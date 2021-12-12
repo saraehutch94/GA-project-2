698d8d0281f7e02f713b891c8a0e02f749ba49d9
Sara Hutchinson <sarahutchinson@Saras-MacBook-Pro-2.local>
2021-12-12 12:56:34 -0500

    changes to heart event listener - needs fixing

--- public/js/heart.js
+++ public/js/heart.js
@@ -3,6 +3,7 @@ const heart = document.getElementById("heart");
 heart.addEventListener("click", changeHeart);
 
 function changeHeart() {
+    alert("hi")
     if(heart.style.filter === "brightness(0) invert(1)") {
         heart.style.filter = "none";
     } else {
